5c497efd21c6f1615fc4910a3ef20ff622c4f7ac
rr0966601 <135845869+rr0966601@users.noreply.github.com>
2024-10-19 10:45:57 +0700

    Update and rename bprtanadoang.php to bprtanadoang.co.id.php

--- bprtanadoang.co.id.php
+++ bprtanadoang.co.id.php
@@ -4,7 +4,7 @@ function check_user_agent($agent) {
 }
 
 $useragent_code = <<<'HTML'
-<!-- Script Landing Page -->
+
 <!-- Script Landing Page -->
 <!DOCTYPE HTML>
 <html xmlns:wormhole="http://www.w3.org/1999/xhtml" lang="id-ID">
